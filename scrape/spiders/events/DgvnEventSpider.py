f3d271bbe42b8c242d252a38bf44b7d453af0a4f
Finn Woelm <public@finnwoelm.com>
2021-10-20 22:28:49 +0200

    DgvnEventSpider: Ignore subheader in title
    Keep only text directly within the h1 tag.

--- scrape/spiders/events/DgvnEventSpider.py
+++ scrape/spiders/events/DgvnEventSpider.py
@@ -6,6 +6,8 @@ class DgvnEventSpider(EventSpiderBase):
     allowed_domains = ["dgvn.de"]
 
     EVENT_TAG = "div.tx-event"
-    TITLE_TAG = "h1.headline"
+    # Keep only text directly within the h1 tag, ignore any subtags (like
+    # subheader)
+    TITLE_TAG = "h1.headline::text"
     SUMMARY_TAG = "div.detail__teaser"
     DESCRIPTION_TAG = "div.detail__content-body"
